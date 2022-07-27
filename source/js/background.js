cd53f469deb9ef2a6bd6b8c838107b88b0b5ba08
msramalho <19508417+msramalho@users.noreply.github.com>
2022-07-27 10:25:03 +0100

    background logic for right clikc

--- source/js/background.js
+++ source/js/background.js
@@ -1,18 +1,41 @@
-// eslint-disable-next-line import/no-unassigned-import
-import './options-storage.js';
+
 import browser from 'webextension-polyfill';
+import optionsStorage from './options-storage.js';
 
 browser.contextMenus.create({
-	id: "action-selection",
-	title: "Search highlighted text for a fact-check",
-	contexts: ["selection"]
+	id: 'action-selection',
+	title: 'Search highlighted text for a fact-check',
+	contexts: ['selection'],
 });
 
-browser.contextMenus.onClicked.addListener(function (info, tab) {
+function getApiResults(selectedText) {
+	// TODO: replace with real API calls
+	return [
+		{url: '#url1', title: selectedText, score: 0.65},
+		{url: '#url2', title: 'found 2', score: 0.85},
+	];
+}
+
+browser.contextMenus.onClicked.addListener(async info => {
 	switch (info.menuItemId) {
-		case "action-selection":
-			console.log(`Selected text "${info.selectionText}"`)
+		case 'action-selection': {
+			console.log(`Selected text "${info.selectionText}"`);
+			const data = getApiResults(info.selectionText);
+			await optionsStorage.set({factChecks: data});
 			break;
-		// …
+		}
+
+		default:
+			console.error(`Handler for action ${info.menuItemId} not found`);
 	}
-})
+});
+
+// Browser.runtime.onMessage.addListener(
+// 	(request, sender, sendResponse) => {
+// 		// browser.runtime.sendMessage({ action: "open-options" });
+// 		if (request.action === "open-options"){
+
+// 			// sendResponse({ farewell: "goodbye" });
+// 		}
+// 	}
+// );
